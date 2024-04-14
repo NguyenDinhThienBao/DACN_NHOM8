2ab9eb54f3f5da476c83e9a77a4dd14780a39437
NguyenDinhThienBao <21520635@gm.uit.edu.vn>
2024-04-14 22:26:58 +0700

    install nodejs expressjs

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/*back end*/ /*======tải express.js==========*/
+const express = require('express') /*lấy express trong node_modules để nạp vào const express*/ 
+const app = express() /*gọi express để trả lại instance (trả lại 1 đối tượng đại diện cho ứng dụng nodejs)*/ 
+const port = 3000 //port website
+//đinh nghĩa route / 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+/*===============================================*/
 
 
