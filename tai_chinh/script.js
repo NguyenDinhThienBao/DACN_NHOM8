16550f3a5b21f79557c390e54e87c61a9ddb0862
NguyenDinhThienBao <21520635@gm.uit.edu.vn>
2024-04-16 11:52:09 +0700

    update chart

---
+++ tai_chinh/script.js
@@ -0,0 +1,28 @@
+const chart = document.querySelector("#chart").getContext('2d');
+
+//Tạo 1 chart instance
+new Chart(chart, {
+    type: 'line',
+    data: {
+        labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+
+        datasets: [
+            {
+                label: '2023',
+                data: [2000, 5500, 11000, 22000],
+                borderColor: 'red',
+                borderWidth: 2
+            },
+
+            {
+                label: '2024',
+                data: [5000, 7000, 12500, 25000],
+                borderColor: 'red',
+                borderWidth: 2
+            }
+        ]
+    },
+    options: {
+        responsive: true
+    }
+})
