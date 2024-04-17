51ebabe2593111a39bc2d40d1a9b9cb88be4371f
NguyenDinhThienBao <21520635@gm.uit.edu.vn>
2024-04-17 12:33:23 +0700

    update css khachhang taichinh

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*back end*/ /*======tải express.js==========*/
 const express = require('express'); /*lấy express trong node_modules để nạp vào const express*/ 
 const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+//const handlebars = require('express-handlebars');
 const app = express(); /*gọi express để trả lại instance (trả lại 1 đối tượng đại diện cho ứng dụng nodejs)*/ 
 const port = 3000; //port website
 
@@ -9,8 +9,8 @@ const port = 3000; //port website
 app.use(morgan('combined'));
 
 //template engine
-app.engine('handlebars', handlebars()); //app dùng template engine là handlebars, name đặt là handlebars, lấy thư viện đã khai báo ở trên thả vào handlebars()
-app.set('view engine', 'handlebars'); //set view engine dùng hanlebars, set-đặt cho ứng dụng express sử dụng view engine và handlebars, dùng handlebars ở dòng trên
+//app.engine('handlebars', handlebars()); //app dùng template engine là handlebars, name đặt là handlebars, lấy thư viện đã khai báo ở trên thả vào handlebars()
+//app.set('view engine', 'handlebars'); //set view engine dùng hanlebars, set-đặt cho ứng dụng express sử dụng view engine và handlebars, dùng handlebars ở dòng trên
 
 //đinh nghĩa route / 
 app.get('/', (req, res) => {
