0327a02a17db24a5206923cee2220886f673b818
Trần Phước Chung <128772750+tpchungg@users.noreply.github.com>
2024-05-11 22:38:57 +0700

    update employee model/json

--- src/routes/script.js
+++ src/routes/script.js
@@ -1,13 +1,14 @@
-const express = require('express'); /*lấy express trong node_modules để nạp vào const express*/ 
-const app = express(); /*gọi express để trả lại instance (trả lại 1 đối tượng đại diện cho ứng dụng nodejs)*/ 
-const employeeRouter = require('./employee.js'); //lấy file index.js trong thư mục routes
+const employeeRouter = require('./employee'); //lấy file index.js trong thư mục routes
 const customerRouter = require('./customer.js'); //lấy file index.js trong thư mục routes
 const financeRouter = require('./finance.js'); //lấy file index.js trong thư mục routes
 const manufactureRouter = require('./manufacture.js'); //lấy file index.js trong thư mục routes
 const purchaseRouter = require('./purchase.js'); //lấy file index.js trong thư mục routes
 const siteRouter = require('./site.js'); //lấy file index.js trong thư mục routes
 function route(app) {
-    app.use('/nhan-vien', employeeRouter); //EmployeeController.index là hàm index trong file index.js trong thư mục controllers nó đã bao gồm req và res trong đó
+    app.get('/search', (req, res) => {
+        res.send('Hello');
+     });
+    app.get('/nhan-vien', employeeRouter); //EmployeeController.index là hàm index trong file index.js trong thư mục controllers nó đã bao gồm req và res trong đó
     app.use('/khach-hang', customerRouter); 
     app.use('/tai-chinh', financeRouter);
     app.use('/san-xuat', manufactureRouter);
