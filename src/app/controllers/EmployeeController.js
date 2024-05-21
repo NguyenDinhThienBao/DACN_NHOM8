9777c577d3ad4966188393396e0cdd9add139806
Trần Phước Chung <128772750+tpchungg@users.noreply.github.com>
2024-05-22 05:36:39 +0700

    update employee list
    đọc dữ liệu từ database rồi hiển thị lên giao diện chính của nhân viên

--- src/app/controllers/EmployeeController.js
+++ src/app/controllers/EmployeeController.js
@@ -2,10 +2,13 @@ const { render } = require('node-sass');
 const Employee = require('../models/Employee');
 class EmployeeController {
   // GET /nhan-vien
-  async index(req, res) {
-    res.render('employee');
-    // const employee = await Employee.find({});
-    // res.json(employee);
+  async index(req, res, next) {
+    Employee.find({})
+    .then(employee => {
+      employee = employee.map(employee => employee.toObject())
+      res.render('employee', { employee });
+    })
+    .catch(error => next());
   }
 }
 module.exports = new EmployeeController;
