bff31687f4bb5905f84b02c7d9aaba4061f02b1e
Trần Phước Chung <128772750+tpchungg@users.noreply.github.com>
2024-05-22 04:10:24 +0700

    update frontend to server
    hiển thị được giao diện trên server nodejs, chuyển tab được trên thanh navigation board, link đc hbs và scss

--- src/routes/script.js
+++ src/routes/script.js
@@ -1,14 +1,13 @@
-const employeeRouter = require('./employee'); //lấy file index.js trong thư mục routes
+const employeeRouter = require('./employee.js'); //lấy file index.js trong thư mục routes
 const customerRouter = require('./customer.js'); //lấy file index.js trong thư mục routes
 const financeRouter = require('./finance.js'); //lấy file index.js trong thư mục routes
 const manufactureRouter = require('./manufacture.js'); //lấy file index.js trong thư mục routes
 const purchaseRouter = require('./purchase.js'); //lấy file index.js trong thư mục routes
 const siteRouter = require('./site.js'); //lấy file index.js trong thư mục routes
+const { engine } = require ('express-handlebars');
+const path = require('path');
 function route(app) {
-    app.get('/search', (req, res) => {
-        res.send('Hello');
-     });
-    app.get('/nhan-vien', employeeRouter); //EmployeeController.index là hàm index trong file index.js trong thư mục controllers nó đã bao gồm req và res trong đó
+    app.use('/nhan-vien', employeeRouter); //EmployeeController.index là hàm index trong file index.js trong thư mục controllers nó đã bao gồm req và res trong đó
     app.use('/khach-hang', customerRouter); 
     app.use('/tai-chinh', financeRouter);
     app.use('/san-xuat', manufactureRouter);
