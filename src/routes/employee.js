c17a6ae1cca40a2c8b54b9d06ef64ee5c3af24bc
Trần Phước Chung <128772750+tpchungg@users.noreply.github.com>
2024-06-01 00:54:45 +0700

    update add nhân viên
    sai đường dẫn đến sao lưu => đã fix

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const employeeController = require('../app/controllers/EmployeeController'); //lấy file index.js trong thư mục controllers
+router.get('/them-nhan-vien', employeeController.add);
 router.put('/:id', employeeController.update);
 router.get('/:id/edit', employeeController.edit);
 router.post('/luu-nhan-vien', employeeController.store);
-router.get('/them-nhan-vien', employeeController.add);
 router.get('/:slug', employeeController.show);
 router.use('/', employeeController.index); //EmployeeController.index là hàm index trong file index.js trong thư mục controllers nó đã bao gồm req và res trong đó 
 module.exports = router; //export router để file khác có thể sử dụng được
