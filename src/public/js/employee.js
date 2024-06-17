d7bf4e18e5e01cce125c7ccc60f5fc78976411d1
Trần Phước Chung <128772750+tpchungg@users.noreply.github.com>
2024-06-18 05:01:15 +0700

    fix ThamNienLamViec

--- src/public/js/employee.js
+++ src/public/js/employee.js
@@ -1,3 +1,13 @@
+//Tìm kiếm
+const searchForm = document.getElementById('employee-search');
+searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const searchResult = document.getElementById('search-text').value;
+    const response = await fetch(`/nhan-vien/tim-kiem?search-query=${searchResult}`,{method: 'GET',});
+    const data = await response.json();
+});
+
+//Bộ lọc
 const filterForm = document.getElementById('filter');
 filterForm.addEventListener('submit', async (event) => {
   event.preventDefault();
@@ -14,3 +24,5 @@ filterForm.addEventListener('submit', async (event) => {
   const data = await response.json();
   // Xử lý dữ liệu trả về và hiển thị kết quả lọc
 });
+
+
