4ebca3b1a54beb3a1903030c3bd5ce43ab96bf7a
Trần Phước Chung <128772750+tpchungg@users.noreply.github.com>
2024-07-05 20:04:46 +0700

    Update ManufactureController.js

--- src/app/controllers/ManufactureController.js
+++ src/app/controllers/ManufactureController.js
@@ -3,33 +3,13 @@ const Manufacture = require('../models/Manufacture');
 const { multipleMongooseToObject } = require('../../util/mongoose');
 const Purchase = require('../models/Purchase');
 class ManufactureController {
-  // async index(req, res, next) {
-  //   Manufacture.find({})
-  //   .then(manufacture => {
-  //     manufacture = manufacture.map(manufacture => manufacture.toObject())
-  //     res.render('manufacture', { manufacture });
-  //   })
-  //   .catch(error => next());
-  // }
   async index(req, res, next) {
-    try {
-      const page = parseInt(req.query.page) || 1; // lấy trang hiện tại từ query string, mặc định là 1
-      const limit = 9; // số lượng sản phẩm hiển thị trên mỗi trang
-      const startIndex = (page - 1) * limit;
-      const endIndex = page * limit;
-  
-      const manufacture = await Manufacture.find({}).skip(startIndex).limit(limit);
-      const totalItems = await Manufacture.countDocuments();
-      const totalPages = Math.ceil(totalItems / limit);
-  
-      res.render('manufacture', {
-        manufacture: manufacture.map(item => item.toObject()),
-        currentPage: page,
-        totalPages: totalPages,
-      });
-    } catch (error) {
-      next(error);
-    }
+    Manufacture.find({})
+    .then(manufacture => {
+      manufacture = manufacture.map(manufacture => manufacture.toObject())
+      res.render('manufacture', { manufacture });
+    })
+    .catch(error => next());
   }
   
   //[POST]
