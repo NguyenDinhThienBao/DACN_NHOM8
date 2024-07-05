91583b7dfb364070bb49053bc862f4d096893f15
NguyenDinhThienBao <21520635@gm.uit.edu.vn>
2024-07-05 15:47:03 +0700

    update Purchase #2

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -13,6 +13,16 @@ class PurchaseController {
     .catch(error => next());
   }
 
+  // Hiển thị chi tiết sản phẩm khi click vào sản phẩm với slug là mã sản phẩm
+  async show(req, res, next) {
+    Purchase.findOne({ MaSP: req.params.slug })
+    .then(purchaseDetail => {
+      purchaseDetail = purchaseDetail.toObject();
+      res.render('purchaseDetail', {purchaseDetail});
+    })
+    .catch(error => next());
+  }
+
   //GET /mua-hang/hoa-don
   async show(req, res, next) {
     Purchase.find({})
@@ -25,13 +35,34 @@ class PurchaseController {
   }
 
   //ADD /hoa-don/tao-hoa-don
-  async add(req, res, next){
+  /*async add(req, res, next){
     res.render('createBill');
-  }
+  }*/
 
   //ADD /mua-hang/them-san-pham
   async add(req, res, next){
     res.render('addItems');
+  }
+
+  //[POST]
+  async store(req, res, next){
+    const purchase = new Purchase(req.body);
+    if(req.file){
+      purchase.AnhSP = req.file.filename;
+    }
+    purchase.save()
+    .then(() => res.redirect('/mua-hang'))
+    .catch(error => next());
+  }
+
+
+
+
+  // [DELETE]
+  async delete(req, res, next){
+    Purchase.deleteOne({_id: req.params.id})
+    .then(() => res.redirect('back'))
+    .catch(error => next());
   }
 
 }
